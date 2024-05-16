f7846d731477152370248afe1322a88af05154e8
lukas <lukas.lamim@hotmail.com>
2024-05-16 14:35:58 -0300

    finalizando projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
+    public ContaTerminal(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, Digite o numero da sua agencia:");
+        int agencia = scanner.nextInt();
+
+        scanner = new Scanner(System.in);
+        System.out.println("Por favor, Digite o numero da sua conta:");
+        String conta = scanner.nextLine();
+
+        scanner = new Scanner(System.in);
+        System.out.println("Por favor, Digite o seu nome:");
+        String nome = scanner.nextLine();
+
+        scanner = new Scanner(System.in);
+        System.out.println("Por favor, Digite o saldo inicial para depositar:");
+        float saldo = scanner.nextFloat();
+
+        String mensagemFinal = "ola, " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque";
+
+        System.out.println(mensagemFinal);
+    }
 }
